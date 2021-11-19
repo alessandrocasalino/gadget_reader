cc25d9fe42425f682aee8180b30407fd9bec77e3
Alessandro Casalino <alessandro.casalino@unibo.it>
2021-11-19 18:09:43 +0100

    Indentation

--- read_gadget.py
+++ read_gadget.py
@@ -13,9 +13,9 @@ filename = "/scratch/extra/marco.baldi5/Alessandro/C-Gadget_examples/output_lcdm
 # ------------ FILE IMPORT ------------
 # Open the file if available
 def open_snapshot(filename, nfile = 0):
-    
+
     import os
-    
+
     # Check the existance of the file
     if os.path.exists(filename):
           curfilename = filename
@@ -32,7 +32,7 @@ def open_snapshot(filename, nfile = 0):
 
     # Open the file in read mode
     snap = open(curfilename, 'rb')
-    
+
     return snap
 
 
@@ -58,7 +58,7 @@ def check_blocksize(snap):
         return -1
 
 
-    
+
 # ------------ HEADER ------------
 # Read the header informations
 # For informations about the structure check the Gadget-2 user guide
@@ -66,17 +66,17 @@ def check_blocksize(snap):
 # - snap               : snapshot file
 # - print_informations : print header informations
 def read_header(filename, print_informations = True):
-    
+
     # Open snapshot
     snap = open_snapshot(filename)
     if not snap:
         return
-    
+
     # Check if the blocksize of the header is 256, and the endian
     swap = check_blocksize(snap)
     if swap == -1:
         return
-    
+
     # Number of particles (array by type) IN THIS FILE
     # 0 - Gas particles
     # 1 - Halo particles
@@ -123,7 +123,7 @@ def read_header(filename, print_informations = True):
 
     # Hubble parameter h
     h = np.fromfile(snap,dtype=np.float64,count=1)[0]
-    
+
     # Swap for correct small/big-endian
     if swap == 1:
         npart_file.byteswap(True)
@@ -139,11 +139,11 @@ def read_header(filename, print_informations = True):
         Omega_cdm = Omega_cdm.byteswap(True)
         Omega_Lambda = Omega_Lambda.byteswap(True)
         h = h.byteswap(True)
-        
+
     snap.close()
-    
+
     Nparticles = np.sum(npart_sim)
-    
+
     # Print informations about the simulation
     if print_informations:
         print("Simulation informations")
@@ -153,7 +153,7 @@ def read_header(filename, print_informations = True):
         print("- h:                   ", h)
         print("- Omega_cdm:           ", Omega_cdm)
         print("- Omega_Lambda:        ", Omega_Lambda)
-    
+
     return nfile, swap
 
 
@@ -166,9 +166,9 @@ def read_header(filename, print_informations = True):
 # NOTE: the particles are stored in order of type, so in principle to extract only one type you might need
 #       n_part_file on each snapshot file
 def extract_block(filename, block, debug = False):
-    
+
     nfile, swap = read_header(filename, print_informations = False)
-    
+
     # Block to extract:  1 for POS, 2 for VEL
     # We also set the data type of the data to extract
     # For the whole list of available blocks, check:
@@ -188,9 +188,9 @@ def extract_block(filename, block, debug = False):
     else:
         print("No Gadget compatible block selected!")
         return None
-    
+
     data = np.zeros(0, dtype = data_type)
-    
+
     # We loop the different files related to the same snapshot
     for i in range(nfile):
 
@@ -201,31 +201,31 @@ def extract_block(filename, block, debug = False):
         snap.seek(0, os.SEEK_END)   # Go to the end
         filesize = snap.tell()      # Current file position
         snap.seek(0, os.SEEK_SET)   # Go to the beginning
-        
+
         block_num = 0
-        
+
         # We loop until we reach the end of the file
         # DATA STRUCTURE while using the pointer
         # blocksize (4 bytes - int32) --- data (data_type * number of data) --- blocksize (4 bytes - int32)
         while(snap.tell()<filesize):
-            
+
             # Check the blocksize (pointer advancing by int32 size, i.e. 4 bytes)
             blocksize = np.fromfile(snap, dtype = np.int32,count = 1)[0]
-            
+
             if block_num == block_wanted:
-                
+
                 if debug: print("Find block", block, " in snapshot", i ,". Reading...")
-                
+
                 # We append the whole block of data (we split them in 3d arrays later if POS and VEL)
                 # NOTE: This is faster than splitting data here with a loop
                 data = np.append(data, np.fromfile(snap, dtype = data_type, count = int(blocksize/np.dtype(data_type).itemsize)))
-                
+
                 if debug: print("Reading complete.")
-                
+
                 break
 
             else:
-                
+
                 # If this is not the data block we have to advance the pointer to the blocksize
                 snap.seek(blocksize, os.SEEK_CUR)
                 # But we also have to advance it for the final blocksize int
@@ -236,19 +236,19 @@ def extract_block(filename, block, debug = False):
                     print("Something is wrong!")
 
             block_num = block_num + 1
-        
+
         snap.close()
-    
+
     # Check for little/big endian
     if swap == 1:
         data.byteswap(True)
-    
+
     # Finally we split the data to have 3d arrays if POS or VEL
     if block in ["POS", "VEL"]:
         if debug: print("Splitting data...")
         data = np.split(data,len(data)/3)
         if debug: print("Splitting complete...")
-    
+
     return data
 
 
